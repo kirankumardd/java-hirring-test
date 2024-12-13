10adfcf3ebfba36c9184a9dd22b54b53f3174983
kirankumardd <58466233+kirankumardd@users.noreply.github.com>
2024-12-13 14:28:02 +0530

    Create Main.java

---
+++ Two-Sum/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Main {
+
+    private static int[] twoSum(int[] nums, int target) {
+        return new int[]{}; // TODO implement this
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test 1: " + Arrays.equals(twoSum(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1}));
+        System.out.println("Test 2: " + Arrays.equals(twoSum(new int[]{3, 2, 4}, 6), new int[]{0, 2}));
+        System.out.println("Test 3: " + Arrays.equals(twoSum(new int[]{3, 3}, 6), new int[]{0, 1}));
+    }
+}
